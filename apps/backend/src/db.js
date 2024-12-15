cfad31855a54d60e90d79fe4f6779a6816056808
Sono Dennis Willbrand <so.willbrand@gmail.com>
2024-12-15 18:59:35 +0100

    prod ready

--- apps/backend/src/db.js
+++ apps/backend/src/db.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
 export async function connectToDB() {
-  const MONGODB_URL = process.env.MONGODB_URL;
-  if (!MONGODB_URL) {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  const DB_USERNAME = process.env.DB_USERNAME;
+  const DB_PASSWORD = process.env.DB_PASSWORD;
+  if (!MONGODB_URI) {
     throw new Error("Error: ENV not existing");
   }
-  await mongoose.connect(MONGODB_URL);
+  await mongoose.connect(`mongodb://${MONGODB_URI}`, {
+    user: DB_USERNAME,
+    pass: DB_PASSWORD,
+  });
   console.log("Connected!");
 }
