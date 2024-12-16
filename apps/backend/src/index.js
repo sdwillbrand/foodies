af7658459abc7dbfdbc5dfc92d2085f299ab2cc5
Sono Dennis Willbrand <so.willbrand@gmail.com>
2024-12-16 12:48:23 +0100

    fixing cors

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -8,25 +8,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const ORIGIN = process.env.ORIGIN || "http://localhost:5173";
+const corsOptions = {
+  credentials: true,
+  origin: [ORIGIN],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
 const app = express();
 // Middleware stack
-app.use(
-  cors({
-    credentials: true,
-    origin: [ORIGIN],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 app.use(cookieParser());
 app.use(express.json());
 
 app.get("/api/health", (req, res) => res.send("Ok"));
 app.use("/api/recipes", recipeRouter);
 app.use("/api/auth", authRouter);
-
-app.use("/api/uploads", express.static("uploads"));
 
 app.use((error, request, response, next) => {
   console.error(error);
